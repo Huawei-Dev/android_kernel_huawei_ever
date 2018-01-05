c9643f60216b7ee99f0e55a6a4e98fb1c29066e1
Eric Biggers <ebiggers@google.com>
2018-01-05 10:44:59 -0800

    fscrypt: move fscrypt_is_dot_dotdot() to fs/crypto/fname.c
    Only fs/crypto/fname.c cares about treating the "." and ".." filenames
    specially with regards to encryption, so move fscrypt_is_dot_dotdot()
    from fscrypt.h to there.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- include/linux/fscrypt_common.h
+++ include/linux/fscrypt_common.h
@@ -183,17 +183,6 @@ static inline bool fscrypt_valid_enc_modes(u32 contents_mode,
 	return false;
 }
 
-static inline bool fscrypt_is_dot_dotdot(const struct qstr *str)
-{
-	if (str->len == 1 && str->name[0] == '.')
-		return true;
-
-	if (str->len == 2 && str->name[0] == '.' && str->name[1] == '.')
-		return true;
-
-	return false;
-}
-
 static inline struct page *fscrypt_control_page(struct page *page)
 {
 #if IS_ENABLED(CONFIG_FS_ENCRYPTION)
